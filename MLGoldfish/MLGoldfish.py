0c7c4dfd462b5fb9b9c37c79ef1b725b51e0e560
SAKURAI Tayra <taira_sakurai@outlook.jp>
2026-03-22 18:57:34 +0900

    modified some.

--- MLGoldfish/MLGoldfish.py
+++ MLGoldfish/MLGoldfish.py
@@ -1,3 +1,17 @@
 ﻿import tensorflow as tf
-import ultralytics
+from ultralytics import YOLO
 import cv2
+from pathlib import Path
+
+model = YOLO("yolo26n.pt")
+image_path = Path("C:\\Users\\taira\\Pictures\\カメラ ロール\\Goldfish")
+
+for path in image_path.glob("**/*.jpg"):
+    cv = cv2.imread(str(path))
+    print(f'Image "{path.name}" was loaded.')
+    if cv is None:
+        continue
+    resultL = model(cv, save_dir="result")
+    for result in resultL:
+        result.save_crop("C:\\Users\\taira\\source\\repos\\MLGoldfish\\MLGoldfish\\result")
+        print("Saved!")
